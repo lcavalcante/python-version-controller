af32d5d7b0b8e2dcb1f94327c529fdf95ccce610
Lucas Cavalcante <5858507+lcavalcante@users.noreply.github.com>
2025-05-10 16:27:44 -0300

    fix: bump not clearing predecessor (#2)
    * chore: add logs
    
    * fix: bump not clearing predecessor
    
    * added pytest
    * added fixtures for commits
    * refactored bump.py from main.py
    
    ---------
    
    Signed-off-by: lcavalcante <5858507+lcavalcante@users.noreply.github.com>

--- pyvc/main.py
+++ pyvc/main.py
@@ -1,41 +1,8 @@
-from enum import StrEnum
+import structlog
+from bump import bump_version, print_semver
 
 
-class CommitEnum(StrEnum):
-    FEAT = "feat"
-    FIX = "fix"
-    BUILD = "build"
-    CHORE = "chore"
-    CI = "ci"
-    DOCS = "docs"
-    STYLE = "style"
-    REFACTOR = "refactor"
-    PERF = "perf"
-    TEST = "test"
-    BREAKING = "breaking change"
-
-
-def is_breaking_change(type: str, message: str) -> bool:
-    return type[-1] == "!" or "BREAKING CHANGE:" in message
-
-
-def print_semver(semver: dict) -> str:
-    return f"{semver.get('major', 0)}.{semver.get('minor', 0)}.{semver.get('patch')}"
-
-
-def bump_version(type: str, message: str, semver: dict) -> None:
-    if is_breaking_change(type, message):
-        semver["major"] += 1
-        print(f"{type} commit bump MAJOR")
-    elif type == CommitEnum.FEAT:
-        semver["minor"] += 1
-        print(f"{type} commit bump MINOR")
-    elif type == CommitEnum.FIX:
-        semver["patch"] += 1
-        print(f"{type} commit bump PATCH")
-    else:
-        print(f"{type} commit has no bump")
-
+log = structlog.get_logger()
 
 mock_commits = [
     "chore: initial commit",
@@ -57,13 +24,9 @@ def main():
     }
 
     for message in mock_commits:
-        head = message.split("\n")[0]
-        parsed_head = head.split(":")
-        if len(parsed_head) > 1:
-            commit_type = parsed_head[0]
-            bump_version(commit_type, message, semver)
+        bump_version(message, semver)
 
-        print(f"final version: {print_semver(semver)}")
+    log.info(f"final version {print_semver(semver)}")
 
 
 if __name__ == "__main__":
---
+++ tests/bump/test_breaking_change.py
@@ -0,0 +1,41 @@
+from pyvc.bump import is_breaking_change
+
+
+def test_feat_normal(feat_commit):
+    commit_type, message = feat_commit
+    assert is_breaking_change(commit_type, message) is False
+
+
+def test_fix_normal(fix_commit):
+    commit_type, message = fix_commit
+    assert is_breaking_change(commit_type, message) is False
+
+
+def test_feat_with_exclamation(feat_breaking_with_exclamation):
+    commit_type, message = feat_breaking_with_exclamation
+    assert is_breaking_change(commit_type, message) is True
+
+
+def test_fix_with_exclamation(fix_breaking_with_exclamation):
+    commit_type, message = fix_breaking_with_exclamation
+    assert is_breaking_change(commit_type, message) is True
+
+
+def test_with_breaking_change_text(feat_breaking_with_text):
+    commit_type, message = feat_breaking_with_text
+    assert is_breaking_change(commit_type, message) is True
+
+
+def test_chore_normal(chore_commit):
+    commit_type, message = chore_commit
+    assert is_breaking_change(commit_type, message) is False
+
+
+def test_docs_normal(docs_commit):
+    commit_type, message = docs_commit
+    assert is_breaking_change(commit_type, message) is False
+
+
+def test_chore_with_exclamation():
+    commit_type, message = ("chore!", "Major dependency update")
+    assert is_breaking_change(commit_type, message) is True
