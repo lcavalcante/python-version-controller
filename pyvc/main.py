44883d18c24fcc7fd885a635dd11479bd35f68bd
lcavalcante <5858507+lcavalcante@users.noreply.github.com>
2025-05-10 16:24:48 -0300

    fix: bump not clearing predecessor
    * added pytest
    * added fixtures for commits
    * refactored bump.py from main.py
    
    Signed-off-by: lcavalcante <5858507+lcavalcante@users.noreply.github.com>

--- pyvc/main.py
+++ pyvc/main.py
@@ -1,56 +1,8 @@
-from enum import StrEnum
 import structlog
+from bump import bump_version, print_semver
+
 
 log = structlog.get_logger()
-
-
-class CommitEnum(StrEnum):
-    FEAT = "feat"
-    FIX = "fix"
-    BUILD = "build"
-    CHORE = "chore"
-    CI = "ci"
-    DOCS = "docs"
-    STYLE = "style"
-    REFACTOR = "refactor"
-    PERF = "perf"
-    TEST = "test"
-    BREAKING = "breaking change"
-
-
-def is_breaking_change(type: str, message: str) -> bool:
-    return type[-1] == "!" or "BREAKING CHANGE:" in message
-
-
-def print_semver(semver: dict) -> str:
-    return f"{semver.get('major', 0)}.{semver.get('minor', 0)}.{semver.get('patch')}"
-
-
-def bump_version(message: str, semver: dict, log=log) -> None:
-    head = message.split("\n")[0]
-
-    # TODO: regex?
-    parsed_head = head.split(":")
-
-    if len(parsed_head) > 1:
-        commit_type = parsed_head[0].strip()
-        head_text = parsed_head[1].strip()
-        log = log.bind(tag=commit_type, text=head_text)
-
-        if is_breaking_change(commit_type, message):
-            semver["major"] += 1
-            log.debug("bump Major")
-        elif commit_type == CommitEnum.FEAT:
-            semver["minor"] += 1
-            log.debug("bump Minor")
-        elif commit_type == CommitEnum.FIX:
-            semver["patch"] += 1
-            log.debug("bump Patch")
-        else:
-            log.debug("no bump")
-    else:
-        log.info("not in conventional commit spec")
-
 
 mock_commits = [
     "chore: initial commit",
